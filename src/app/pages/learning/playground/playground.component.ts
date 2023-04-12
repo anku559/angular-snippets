b47185f2f43e011c251984f409c473c53ed94793
Ankit Singh <ankitsingh91@gmail.com>
2023-04-12 11:09:03 +0530

    Playground Component

---
+++ src/app/pages/learning/playground/playground.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-playground',
+  templateUrl: './playground.component.html',
+  styleUrls: ['./playground.component.scss'],
+})
+export class PlaygroundComponent {}
